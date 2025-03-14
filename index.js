51b62d5ed04769c6dc98caec24216df25523cdee
Mustafa Fedayi <mustafafedayi@macbookpro.myfiosgateway.com>
2025-03-14 13:28:33 -0400

    adding html and js files

---
+++ index.js
@@ -0,0 +1,9 @@
+// Below is the aritmethic calculations saved in to variables.
+const x = 8 + 2;
+const y = 10 * 4;
+const o = 40 - 1;
+
+// Alert to display the message and value of the variables.
+alert(
+  `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${x}-${y}-${o}`
+);
